39da0e0cd35eb8235f1f494abac45abc0ddabe54
Pablo Vergés <pablo@soofa.ch>
2012-06-20 10:36:13 +0200

    corrected exercise 13.1

--- 13.1.c
+++ 13.1.c
@@ -7,22 +7,17 @@ double fun(double x) {
 	return exp(- x * x / (double) 2) / sqrt((double) 2 * M_PI);
 }
 
-double monte_carlo(double (*f)(double), double variance, double start, double end, double precision) {
+double monte_carlo(double (*f)(double), double start, double end, int steps) {
 	
 	// the counter, the number of steps and the index i
-	long int steps, i;
+	int i;
 	
 	// our random x
 	double x, sum;
 	
 	// set the seed for the random number generator
 	srand(time(NULL));
-	
-	// calculate the amount of steps
-	steps = (long int) (variance/(precision * precision) + 1);
-	
-	printf("number of steps: %li", steps);
-	
+		
 	// set the amount of steps and the sum, set the counter and LOOP!
 	for (i = 0, sum = 0; i < steps; i++) {
 		
@@ -39,10 +34,13 @@ double monte_carlo(double (*f)(double), double variance, double start, double en
 int main() {
 	
 	double result;
+	int steps = 1e6;
 	
-	result = monte_carlo(&fun, 8, -1, 1, 1e-6);
+	result = monte_carlo(&fun, -1, 1, steps);
 	
 	printf("Value of the integral: %g\n", result);
+	printf("The error of the integral is: %g\n", fabs(0.68268949 - result));
+	printf("The theoretical error is: %g\n", (double) 8 / sqrt(steps - 1));
 	
 	return 1;
 }
